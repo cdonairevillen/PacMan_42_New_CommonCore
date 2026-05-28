d52e122c785d1d5417e8b75eb4f6936b60afeece
Chrystian Donaire <cdonairevillen@gmail.com>
2026-05-28 16:43:52 +0200

    Delete src/UI/hud.py

--- src/UI/hud.py
+++
@@ -1,8 +0,0 @@
-class HUD():
-
-    """
-    Me parece un pelin estupido tener un hud dedicado. No se
-    como se integraria con el visualizer del todo. Habria que
-    echarle un ojo.
-    """
-    pass
